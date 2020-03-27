4d7fe5748226d6c150307b9bc4648ec7d0727960
n.isobe <ganmodoki7.43dari@gmail.com>
2020-03-27 17:39:27 +0900

    q8,10修正,q12回答

--- task.rb
+++ task.rb
@@ -65,6 +65,7 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
+  programming_languages.map!(&:capitalize)
   upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
@@ -82,15 +83,15 @@ end
 
 def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
+
+  # 以下に回答を記載
   foods.each do |food|
-    if food == "うに"
+    if food.include?("うに")
       puts "好物です"
     else
       puts "まぁまぁ好きです"
     end
   end
-  # 以下に回答を記載
-
 end
 
 def q11
@@ -107,6 +108,7 @@ def q12
   data = { user: { name: "satou", age: 33 } }
 
   # 以下に回答を記載
+  puts data[:user][:name]
 end
 
 def q13
