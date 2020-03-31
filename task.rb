9155c2f526beb1e210ef1505cd9b5c9e13d94768
n.isobe <ganmodoki7.43dari@gmail.com>
2020-03-31 16:45:48 +0900

    Q13~Q16を回答

--- task.rb
+++ task.rb
@@ -116,14 +116,15 @@ def q13
   update_data = { age: 32, address: "沖縄" }
 
   # 以下に回答を記載
-
+  p user_data.merge!(update_data)
 end
 
 def q14
   data = { name: "satou", age: 33, address: "saitama", hobby: "soccer", email: "hoge@fuga.com" }
 
   # 以下に回答を記載
-
+  data2 = data.values
+  p data2
 end
 
 def q15
@@ -131,7 +132,15 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-
+  def has_key(data)
+    if data.has_key?(:age)
+      puts "OK"
+    else
+      puts "NG"
+    end
+  end
+  print has_key(data1)
+  print has_key(data2)
 end
 
 def q16
@@ -143,7 +152,9 @@ def q16
   ]
 
   # 以下に回答を記載
-
+  users.each do |user|
+    puts "私の名前は#{user[:name]}です。年齢は#{user[:age]}歳です。"
+  end
 end
 
 class UserQ17
